a8c0819c0c38b5db4aedbbffb45829b888b78f27
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-05-02 21:19:10 +0530

    update Navbar

--- Real_estate/src/App.jsx
+++ Real_estate/src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "./Components/Header";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="w-full">
+      <Header />
+    </div>
+  );
 };
 
 export default App;
