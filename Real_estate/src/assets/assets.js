0137385782f607ba7f2e075787bc8ad64921a98f
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-05-03 08:39:15 +0530

    update assets.js

--- Real_estate/src/assets/assets.js
+++ Real_estate/src/assets/assets.js
@@ -1,7 +1,9 @@
 import logo from "./logo.jpg";
 import menu_icon from "./menu_icon.jpg";
+import cross_icon from "./cross_icon.png";
 
 export const assets = {
   logo,
   menu_icon,
+  cross_icon,
 };
