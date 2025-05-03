47e04650a2863696ff1030544ee61d5ca44b0ed5
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-05-03 08:33:06 +0530

    update Navbar

--- Real_estate/src/Components/Navbar.jsx
+++ Real_estate/src/Components/Navbar.jsx
@@ -36,11 +36,20 @@ const Navbar = () => {
           Sign up
         </button>
 
-        <img src={assets.menu_icon} className="md-hidden w-7" alt="" />
+        <img
+          src={assets.menu_icon}
+          className="md-hidden w-7 cursor-pointer"
+          alt=""
+        />
       </div>
 
-      <div>
-        <ul>
+      {/*Mobile-menu*/}
+      <div className="md:hidden fixed w-full right-0 top-0 bottom-0 overflow-hidden bg-white transition-all">
+        <div className="flex justify-end p-6 cursor-pointer">
+          <img src={assets.cross_icon} className="w-6" alt="" />
+        </div>
+
+        <ul className="flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium ">
           <a href="#Header" className="px-4 py-2 rounded-full inline-block">
             Home
           </a>
