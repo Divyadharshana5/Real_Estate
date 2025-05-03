469d55fd133e5fdff418007856d23b7d3c5f9cdc
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-05-03 07:48:30 +0530

    update Navbar

--- Real_estate/src/Components/Header.jsx
+++ Real_estate/src/Components/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
     >
       <Navbar />
 
-      <div>
+      <div className="container text-center mx-auto py-4 px-6 md:px-20 lg:px-32 text-white">
         <h2>Explore homes that fit your dreams</h2>
         <div>
           <a href="">Projects</a>
