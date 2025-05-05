7c878fb7080f72a900dd89b1a692a3ff8a2de62e
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-05-05 18:55:06 +0530

    updated About.jsx

--- Real_estate/src/Components/About.jsx
+++ Real_estate/src/Components/About.jsx
@@ -22,6 +22,29 @@ const About = () => {
           alt=""
           className="w-full sm:w-1/2 max-w-lg"
         />
+        <div className="flex flex-col items-center md:items-start mt-10 text-gray-600">
+          <div className="grid grid-cols-2 gap-6 md:gap-10 w-full 2xl:pr-28">
+            <div>
+              <p className="text-4xl font-medium text-gray-800">10+</p>
+              <p>Years of Excellence</p>
+            </div>
+
+            <div>
+              <p className="text-4xl font-medium text-gray-800">12+</p>
+              <p>Projects Completed</p>
+            </div>
+
+            <div>
+              <p className="text-4xl font-medium text-gray-800">20+</p>
+              <p>Mn. Sq. Ft. Delivered</p>
+            </div>
+
+            <div>
+              <p className="text-4xl font-medium text-gray-800">25+</p>
+              <p>Ongoing Projects</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
