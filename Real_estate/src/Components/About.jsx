aaa184b6ef4997f52d89880951f8c390bc50da53
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-05-05 18:33:40 +0530

    updated About.jsx

--- Real_estate/src/Components/About.jsx
+++ Real_estate/src/Components/About.jsx
@@ -6,10 +6,15 @@ const About = () => {
       className="flex flex-col items-center justify-center container mx-auto p-14 md:px-20 lg:px-32 w-full overflow-hidden"
       id="About"
     >
-      <h1>
-        About <span>Our Brand</span>
+      <h1 className="text-2xl sm:text-4xl font-bold mb-2">
+        About{" "}
+        <span className="underline underline-offset-4 decoration-1 under font-light">
+          Our Brand
+        </span>
       </h1>
-      <p>Passionate About properties,Dedicated to Your Vision</p>
+      <p className="text-gray-500 max-w-80 text-center mb-8">
+        Passionate About properties,Dedicated to Your Vision
+      </p>
     </div>
   );
 };
