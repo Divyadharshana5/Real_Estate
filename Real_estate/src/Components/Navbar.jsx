f07a49dd299d74d15bde58f80ed10bc9ea2216ce
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-05-05 18:01:36 +0530

    update Navbar.jsx

--- Real_estate/src/Components/Navbar.jsx
+++ Real_estate/src/Components/Navbar.jsx
@@ -52,7 +52,12 @@ const Navbar = () => {
         }  right-0 top-0 bottom-0 overflow-hidden bg-white transition-all`}
       >
         <div className="flex justify-end p-6 cursor-pointer">
-          <img src={assets.cross_icon} className="w-6" alt="" />
+          <img
+            onClick={() => setShowMobileMenu(false)}
+            src={assets.cross_icon}
+            className="w-6"
+            alt=""
+          />
         </div>
 
         <ul className="flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium ">
