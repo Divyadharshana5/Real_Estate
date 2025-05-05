dad11f55b81893c5913a85c7bdf30e3ea83fa2fd
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-05-05 19:22:30 +0530

    Inialize Projects.jsx

--- Real_estate/src/App.jsx
+++ Real_estate/src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Header from "./Components/Header";
 import About from "./Components/About";
+import Projects from "./Components/Projects";
+import Testimonials from "./Components/Testimonials";
 
 const App = () => {
   return (
     <div className="w-full overflow-hidden">
       <Header />
       <About />
+      <Projects />
+      <Testimonials />
     </div>
   );
 };
