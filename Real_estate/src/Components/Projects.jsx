c70609debd0ef49d61eae9427708f9b2247b4e6e
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-05-05 19:29:22 +0530

    updated  Projects.jsx

--- Real_estate/src/Components/Projects.jsx
+++ Real_estate/src/Components/Projects.jsx
@@ -2,11 +2,19 @@ import React from "react";
 
 const Projects = () => {
   return (
-    <div>
-      <h1>
-        Projects <span>Completed</span>
+    <div
+      className="container max-auto py-4 pt-20 px-6 md:px-20 lg:px-32 my-20 w-full overflow-hidden"
+      id="Projects"
+    >
+      <h1 className="text-2xl sm:text-4xl font-bold mb-2 text-center">
+        Projects{" "}
+        <span className="underline underline-offset-4 decoration-1 under font-light">
+          Completed
+        </span>
       </h1>
-      <p>Crafting Spaces,Building Legacies-Explore Our Portfolio</p>
+      <p className="text-center text-gray-500 mb-8 max-w-80 mx-auto">
+        Crafting Spaces,Building Legacies-Explore Our Portfolio
+      </p>
     </div>
   );
 };
