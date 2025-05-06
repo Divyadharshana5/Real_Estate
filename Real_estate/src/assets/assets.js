b9a24feb88c4282b3cfe1091279e71e46637f599
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-05-06 18:16:17 +0530

    updated  Projects.jsx

--- Real_estate/src/assets/assets.js
+++ Real_estate/src/assets/assets.js
@@ -2,8 +2,8 @@ import logo from "./logo.jpg";
 import menu_icon from "./menu_icon.jpg";
 import cross_icon from "./cross_icon.png";
 import brand_img from "./brand_img.jpg";
-import left_arrow from "./left_arrow.jpeg";
-import right_arrow from "./right_arrow.png";
+import left_arrow from "./left_arrow.jpg";
+import right_arrow from "./right_arrow.jpg";
 
 export const assets = {
   logo,
