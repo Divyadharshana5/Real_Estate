22f9de83dc417aec5eb912694294b51fa4d287ba
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-05-07 17:57:31 +0530

    Update Contact.jsx

--- Real_estate/src/Components/Testimonials.jsx
+++ Real_estate/src/Components/Testimonials.jsx
@@ -9,7 +9,9 @@ const Testimonials = () => {
     >
       <h1 className="text-2xl sm:text-4xl font-bold mb-2 text-center">
         Customer{" "}
-        <span className="underline underline-offset-4 ">Testimonials</span>
+        <span className="underline underline-offset-4 decoration-1 under font-light ">
+          Testimonials
+        </span>
       </h1>
       <p className="text-center text-gray-500 mb-12 max-w-80 mx-auto">
         Real Stories from Those Who Found Home with Us
